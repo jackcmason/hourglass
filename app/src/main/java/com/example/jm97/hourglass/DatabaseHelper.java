8c24660f0dd165b0703d6687f2326b90d1b540be
Jack Mason <jack.mason@posteo.net>
2019-01-12 12:51:44 +1000

    implemented SQLite helper method

---
+++ app/src/main/java/com/example/jm97/hourglass/DatabaseHelper.java
@@ -0,0 +1,36 @@
+package com.example.jm97.hourglass;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Activities.db";
+    public static final String TABLE_NAME = "activity_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "ACTIVITY";
+    public static final String COL_3 = "CATAGORY";
+    public static final String COL_4 = "STARTTIME";
+    public static final String COL_4 = "ENDTIME";
+
+    public DatabaseHelper(@androidx.annotation.Nullable Context context,
+                          @androidx.annotation.Nullable String name,
+                          @androidx.annotation.Nullable SQLiteDatabase.CursorFactory factory,
+                          int version) {
+
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db =  this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, ACTIVITY TEXT, CATAGORY TEXT, STARTTIME TEXT, ENDTIME TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+}

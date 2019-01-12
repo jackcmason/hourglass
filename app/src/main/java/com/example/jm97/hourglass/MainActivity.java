40a5fe718ffd66fe6a3aea374cf37a520ece2df4
Jack Mason <jack.mason@posteo.net>
2019-01-12 10:44:44 +1000

    toast on button

--- app/src/main/java/com/example/jm97/hourglass/MainActivity.java
+++ app/src/main/java/com/example/jm97/hourglass/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jm97.hourglass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.mainbutton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
